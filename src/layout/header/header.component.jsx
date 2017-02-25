df921c5b00a269f36b91c101e79f2a6859f4378a
Christopher Mosure <cj@webmachine.io>
2017-02-25 16:32:35 -0600

    Working with reducers

--- src/layout/header/header.component.jsx
+++ src/layout/header/header.component.jsx
@@ -10,7 +10,7 @@ export default class Header extends React.Component {
           <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <IndexLink to="/" activeStyle={active} className="navbar-brand">Test</IndexLink>
+          <IndexLink to="/" className="navbar-brand">Test</IndexLink>
           <div className="collapse navbar-collapse collapse show justify-content-end" id="navbarCollapse">
             <ul className="navbar-nav">
               <li className="nav-item">
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers }  from 'redux';
+import forms                from './forms';
 
-/*
 export default combineReducers({
+  form: forms
 })
-*/
